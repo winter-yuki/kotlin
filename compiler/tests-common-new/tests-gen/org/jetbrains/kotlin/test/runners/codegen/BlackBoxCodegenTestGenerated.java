8667a9383f4d2b6f052408a06978e781c80ac5dd
Steven Schäfer <stevenschaefer@google.com>
2022-06-13 16:13:42 +0200

    JVM IR: Check that we generate callable references in tests

--- compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/codegen/BlackBoxCodegenTestGenerated.java
+++ compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/codegen/BlackBoxCodegenTestGenerated.java
@@ -15768,6 +15768,12 @@ public class BlackBoxCodegenTestGenerated extends AbstractBlackBoxCodegenTest {
         }
 
         @Test
+        @TestMetadata("canary.kt")
+        public void testCanary() throws Exception {
+            runTest("compiler/testData/codegen/box/directInvokeOptimization/canary.kt");
+        }
+
+        @Test
         @TestMetadata("capturingLambda.kt")
         public void testCapturingLambda() throws Exception {
             runTest("compiler/testData/codegen/box/directInvokeOptimization/capturingLambda.kt");
