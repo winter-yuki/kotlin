e06f8d457a7ae0e0bb9b7a1266842bcdd345b26d
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2022-06-14 12:37:40 +0200

    fixup! JVM IR: Map inline classes to substituted underlying type

--- compiler/testData/codegen/box/inlineClasses/genericUnderlyingValue/paramGenericSignature.kt
+++ compiler/testData/codegen/box/inlineClasses/genericUnderlyingValue/paramGenericSignature.kt
@@ -4,14 +4,18 @@
 // WITH_STDLIB
 
 @JvmInline
-value class AAAA<T : Any>(val x: List<T>)
+value class AAAA<T : Any>(val x: List<T>) {
+    fun <R: Any> equalsChecks2(x: AAAA<R>) {}
+}
 
 class A {
     fun equalsChecks1(x: AAAA<List<Int>>) {}
 }
 
 fun box(): String {
-    val paramTypes = A::class.java.methods.find { it.name.contains("equalsChecks1") }!!.genericParameterTypes.toList().toString()
-    if (paramTypes != "[java.util.List<? extends java.util.List<java.lang.Integer>>]") return "FAIL: $paramTypes"
+    var paramTypes = A::class.java.methods.find { it.name.contains("equalsChecks1") }!!.genericParameterTypes.toList().toString()
+    if (paramTypes != "[java.util.List<? extends java.util.List<java.lang.Integer>>]") return "FAIL 1: $paramTypes"
+    paramTypes = AAAA::class.java.methods.find { it.name.contains("equalsChecks2") }!!.genericParameterTypes.toList().toString()
+    if (paramTypes != "[java.util.List<? extends T>, java.util.List<? extends R>]") return "FAIL 2: $paramTypes"
     return "OK"
 }
