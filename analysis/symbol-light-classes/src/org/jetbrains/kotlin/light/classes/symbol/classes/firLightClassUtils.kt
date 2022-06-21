34d4f4ca243b942b2abcc66fbb4272dd8747aea0
Jaebaek Seo <jaebaek@google.com>
2022-06-21 20:59:19 +0000

    FIR find usage: correctly set extendsList of FirLightInterfaceClassSymbol
    The existing code does not set the list of bases that
    FirLightInterfaceClassSymbol extends for "extendsList". It collects only
    the set of interfaces for "extendsList" of FirLightInterfaceClassSymbol.
    However, interfaces can "extend" other classes and/or interfaces, but
    they cannot "implement" other interfaces. Therefore, we have to includes
    all interfaces and classes that the child interface extends in
    the "extendsList". Additionally, this commit adds `private fun
    PsiClass.hasSuper(..): Boolean` to FirLightClassBase that returns
    whether one of recursive super classes of the PsiClass is `baseClass`
    or not. This commit lets `isInheritor()` method use
    `PsiClass.hasSuper()`.

--- analysis/symbol-light-classes/src/org/jetbrains/kotlin/light/classes/symbol/classes/firLightClassUtils.kt
+++ analysis/symbol-light-classes/src/org/jetbrains/kotlin/light/classes/symbol/classes/firLightClassUtils.kt
@@ -388,10 +388,13 @@ internal fun FirLightClassBase.createInheritanceList(forExtendsList: Boolean, su
         // We don't have Enum among enums supertype in sources neither we do for decompiled class-files and light-classes
         if (isEnum && this.classId == StandardClassIds.Enum) return false
 
-        val isInterfaceType =
-            (this.classSymbol as? KtClassOrObjectSymbol)?.classKind == KtClassKind.INTERFACE
+        // Interfaces have only extends lists
+        if (isInterface) return forExtendsList
 
-        return forExtendsList == !isInterfaceType
+        val classKind = (classSymbol as? KtClassOrObjectSymbol)?.classKind
+        val isJvmInterface = classKind == KtClassKind.INTERFACE || classKind == KtClassKind.ANNOTATION_CLASS
+
+        return forExtendsList == !isJvmInterface
     }
 
     //TODO Add support for kotlin.collections.
