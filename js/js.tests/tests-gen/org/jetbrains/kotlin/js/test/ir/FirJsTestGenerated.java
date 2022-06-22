4e14140575fae2f2bed4c733a28c668864e60864
Artem Kobzar <artem.kobzar@jetbrains.com>
2022-06-22 12:47:03 +0200

    chore: add miss test inside FirTests.

--- js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/ir/FirJsTestGenerated.java
+++ js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/ir/FirJsTestGenerated.java
@@ -7929,6 +7929,12 @@ public class FirJsTestGenerated extends AbstractFirJsTest {
         }
 
         @Test
+        @TestMetadata("externalValWithOverridenVar.kt")
+        public void testExternalValWithOverridenVar() throws Exception {
+            runTest("js/js.translator/testData/box/native/externalValWithOverridenVar.kt");
+        }
+
+        @Test
         @TestMetadata("inheritanceFromNativeClass.kt")
         public void testInheritanceFromNativeClass() throws Exception {
             runTest("js/js.translator/testData/box/native/inheritanceFromNativeClass.kt");
