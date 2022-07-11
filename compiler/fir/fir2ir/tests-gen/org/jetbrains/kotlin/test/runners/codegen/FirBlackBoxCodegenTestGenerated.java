b1fd6e6a94c54ab7157d182c1ad6a9a1e1a26ad3
Pavel Mikhailovskii <pavel.mikhailovskii@jetbrains.com>
2022-07-11 11:09:49 +0200

    KT-53090 Support -Xlambdas=indy for anonymous functions

--- compiler/fir/fir2ir/tests-gen/org/jetbrains/kotlin/test/runners/codegen/FirBlackBoxCodegenTestGenerated.java
+++ compiler/fir/fir2ir/tests-gen/org/jetbrains/kotlin/test/runners/codegen/FirBlackBoxCodegenTestGenerated.java
@@ -26199,6 +26199,12 @@ public class FirBlackBoxCodegenTestGenerated extends AbstractFirBlackBoxCodegenT
             }
 
             @Test
+            @TestMetadata("anyAnnotationDisablesIndyLambdaGeneration.kt")
+            public void testAnyAnnotationDisablesIndyLambdaGeneration() throws Exception {
+                runTest("compiler/testData/codegen/box/invokedynamic/lambdas/anyAnnotationDisablesIndyLambdaGeneration.kt");
+            }
+
+            @Test
             @TestMetadata("bigArityExtLambda.kt")
             public void testBigArityExtLambda() throws Exception {
                 runTest("compiler/testData/codegen/box/invokedynamic/lambdas/bigArityExtLambda.kt");
@@ -26250,6 +26256,18 @@ public class FirBlackBoxCodegenTestGenerated extends AbstractFirBlackBoxCodegenT
             @TestMetadata("kt52875.kt")
             public void testKt52875() throws Exception {
                 runTest("compiler/testData/codegen/box/invokedynamic/lambdas/kt52875.kt");
+            }
+
+            @Test
+            @TestMetadata("lambdaMetadataFactoryIsNotUsed.kt")
+            public void testLambdaMetadataFactoryIsNotUsed() throws Exception {
+                runTest("compiler/testData/codegen/box/invokedynamic/lambdas/lambdaMetadataFactoryIsNotUsed.kt");
+            }
+
+            @Test
+            @TestMetadata("lambdaMetafactoryIsUsed.kt")
+            public void testLambdaMetafactoryIsUsed() throws Exception {
+                runTest("compiler/testData/codegen/box/invokedynamic/lambdas/lambdaMetafactoryIsUsed.kt");
             }
 
             @Test
